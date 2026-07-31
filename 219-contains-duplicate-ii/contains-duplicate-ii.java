34c7d324db131796fc8da357870b7596472e69e8
Aayush Sapkota <aayush.sapkota2010@gmail.com>
2026-07-31 10:34:50 +0530

    Time: 27 ms (36.57%) | Memory: 111.7 MB (5.20%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -3,22 +3,24 @@ class Solution {
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++)
         {
-        if(map.containsKey(nums[i]))
-        {
-            int idx=map.get(nums[i]);
-          if(i-idx<=k)
-          {
-            return true;
-          }
-          else
-          {
-            map.put(nums[i],i);
-          }
-        }
-        else 
-        {
-            map.put(nums[i],i);
-        }
+            if(map.containsKey(nums[i]))
+            {                   //duplicate found now subtract the index
+            int j=map.get(nums[i]);
+            if(i-j<=k)
+            {
+                return true;
+            }
+            else
+            {
+                map.put(nums[i],i);
+            }
+
+            }
+            else
+            {
+                map.put(nums[i],i);
+
+            }
         }
         return false;
         
