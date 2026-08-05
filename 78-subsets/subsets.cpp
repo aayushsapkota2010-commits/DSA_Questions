d9d3bf1f47488251e64675a379827b433949f303
Aayush Sapkota <aayush.sapkota2010@gmail.com>
2026-08-05 21:43:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (82.88%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-void AllSub(int i, vector<int>& nums, vector<int>& current, vector<vector<int>>& ans)
-{
+
+  void AllSub(vector<int>& nums, int i, vector<int>& current, vector<vector<int>>& ans)
+  {
     if(i==nums.size())
     {
         ans.push_back(current);
         return;
-
     }
+    
     current.push_back(nums[i]);
-    AllSub(i+1,nums,current,ans);
+    AllSub(nums,i+1,current,ans);
 
     current.pop_back();
-    AllSub(i+1,nums,current,ans);
-}
+    AllSub(nums,i+1,current,ans);
+  }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> current;
-        AllSub(0,nums,current,ans);
+        AllSub(nums,0,current,ans);
         return ans;
-
-
-        
     }
 };
