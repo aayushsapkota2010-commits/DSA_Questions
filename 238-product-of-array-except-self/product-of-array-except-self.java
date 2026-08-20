c47cd59b972afba41fc15882d0582ab62aa34d7c
Aayush Sapkota <aayush.sapkota2010@gmail.com>
2026-08-20 11:08:29 +0530

    Time: 2 ms (93.74%) | Memory: 65 MB (81.86%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -10,16 +10,19 @@ class Solution {
         {
             prefix[i]=prefix[i-1]*nums[i-1];
         }
-        
+
         suffix[nums.length-1]=1;
         for(int i=nums.length-2;i>=0;i--)
         {
             suffix[i]=suffix[i+1]*nums[i+1];
         }
+
         for(int i=0;i<nums.length;i++)
         {
             ans[i]=prefix[i]*suffix[i];
         }
+
         return ans;
+        
     }
 }
