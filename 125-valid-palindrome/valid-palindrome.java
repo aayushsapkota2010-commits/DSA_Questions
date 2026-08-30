19e8f24df1665a03a96a125dc4a1c9758ee6ce5b
Aayush Sapkota <aayush.sapkota2010@gmail.com>
2026-08-30 08:00:55 +0530

    Time: 2 ms (99.30%) | Memory: 44.4 MB (63.45%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,17 +2,16 @@ class Solution {
     public boolean isPalindrome(String s) {
         int i=0;
         int j=s.length()-1;
-       
-
-
-        while(i<j)
-        {
-     char left=s.charAt(i);
+        
+        while(i<j){
+        char left=s.charAt(i);
         char right=s.charAt(j);
+
         if(!Character.isLetterOrDigit(left))
         {
             i++;
             continue;
+        
         }
 
         if(!Character.isLetterOrDigit(right))
@@ -20,13 +19,18 @@ class Solution {
             j--;
             continue;
         }
+
         if(Character.toLowerCase(left)!=Character.toLowerCase(right))
         {
             return false;
         }
         i++;
         j--;
+
         }
+
         return true;
+
+        
     }
 }
